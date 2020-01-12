ced50cd32c0e668d40d3a6a82dc54c9337688abd
jsz315 <jsz_thing@126.com>
2020-01-12 22:25:20 +0800

    price

--- js/api.js
+++ js/api.js
@@ -203,6 +203,27 @@ async function rightPostage(){
 	return res.data;
 }
 
+async function rightOpen(){
+	let param = {
+		
+	};
+	
+	let res = await http.get('/api/vip/act/right/open.htm', param);
+	return res.data;
+}
+
+async function rightConfirm(){
+	let param = {
+		orderId: "",
+		smsSendNo: "",
+		smsVerifyCode: "",
+	};
+	
+	let res = await http.get('/api/vip/act/right/confirm.htm', param);
+	return res.data;
+}
+
+
 export default {
 	homePage,
 	isPhoneExists,
@@ -218,5 +239,7 @@ export default {
 	mineSave,
 	product,
 	authentication,
-	rightPostage
+	rightPostage,
+	rightOpen,
+	rightConfirm
 }
